6ae056e91b29a0fde78f2d424c4ea9b3fe63a08e
JerromyC <59486979+JerromyC@users.noreply.github.com>
2022-06-07 14:29:58 -0500

    Updates

---
+++ precache-manifest.86cbe7378b39346436be0e96e52289d1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "78464a4658953fb8dea2",
+    "url": "/orange-pangea/static/css/main.c68beaab.chunk.css"
+  },
+  {
+    "revision": "78464a4658953fb8dea2",
+    "url": "/orange-pangea/static/js/main.78464a46.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/orange-pangea/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "7a685cf232dae70c761a",
+    "url": "/orange-pangea/static/js/2.7a685cf2.chunk.js"
+  },
+  {
+    "revision": "5cf36fc068e28a7494bd",
+    "url": "/orange-pangea/static/js/runtime~main.5cf36fc0.js"
+  },
+  {
+    "revision": "6d0310430936389f5169b6ae54ee9c6e",
+    "url": "/orange-pangea/static/media/undraw_people_re_8spw.6d031043.svg"
+  },
+  {
+    "revision": "0099eac8f12748e7afc65b6741266617",
+    "url": "/orange-pangea/static/media/truck-fast-solid.0099eac8.svg"
+  },
+  {
+    "revision": "24d1543d671733fa6514786ae74a577d",
+    "url": "/orange-pangea/static/media/money-bill-transfer-solid.24d1543d.svg"
+  },
+  {
+    "revision": "7635f20e9f43a32d26d4b807d3a16e95",
+    "url": "/orange-pangea/static/media/sistrix-brands.7635f20e.svg"
+  },
+  {
+    "revision": "78e9e3306449563d9606187dcd8c916c",
+    "url": "/orange-pangea/static/media/undraw_home_cinema_l7yl.78e9e330.svg"
+  },
+  {
+    "revision": "a945b1c4b756cd527036994da4e88fc6",
+    "url": "/orange-pangea/static/media/undraw_empty_cart_co35.a945b1c4.svg"
+  },
+  {
+    "revision": "c9da26269fb769b2ed0c12ea65851d2a",
+    "url": "/orange-pangea/static/media/facebook-f-brands.c9da2626.svg"
+  },
+  {
+    "revision": "29423a6fc8f06c0e3fbcd6ea2dea0edd",
+    "url": "/orange-pangea/static/media/twitter-brands.29423a6f.svg"
+  },
+  {
+    "revision": "a8e8feea4a4be6163656cc40072070c7",
+    "url": "/orange-pangea/static/media/google-brands.a8e8feea.svg"
+  },
+  {
+    "revision": "4735b952ba5fed86503e789b2b564b07",
+    "url": "/orange-pangea/index.html"
+  }
+];

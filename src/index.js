8e33b58612e607b9fdd48dbdbc4ae8205e237a32
JerromyC <59486979+JerromyC@users.noreply.github.com>
2022-06-07 14:57:40 -0500

    Please god just work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-      <App />
+  <StrictMode >
+    <HashRouter base='/'>
+        <App />
+    </HashRouter>
   </StrictMode>
 );
 
